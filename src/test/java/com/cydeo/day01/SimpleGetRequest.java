9cfc9707859ee4739f5b7ab3423b031adb5b88fc
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-03-28 14:39:15 +0300

    day01

--- src/test/java/com/cydeo/day01/SimpleGetRequest.java
+++ src/test/java/com/cydeo/day01/SimpleGetRequest.java
@@ -1,4 +1,23 @@
 package com.cydeo.day01;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
 public class SimpleGetRequest {
+    String url="http://54.145.80.239:8000/api/spartans";  // firstly, adding url from postman
+    @Test
+    public void test1(){
+
+        // send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        // print response status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        // print response body
+        response.prettyPrint();
+
+    }
+
 }
