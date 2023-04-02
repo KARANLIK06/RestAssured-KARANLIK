50af42861324e5390d6879fb9f022c395e8d6f00
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-02 16:47:52 +0300

    day02

--- src/test/java/com/cydeo/day02/HrGetRequests.java
+++ src/test/java/com/cydeo/day02/HrGetRequests.java
@@ -1,4 +1,26 @@
 package com.cydeo.day02;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class HrGetRequests {
+
+    //BeforeAll is a annotation equals to @BeforeClass in testNg, we use with static method name
+    @BeforeAll
+    public static void init(){
+
+        RestAssured.baseURI="http://54.145.80.239:1000/ords/hr";
+
+    }
+
+    @DisplayName("GET request to /regions")
+    @Test
+    public void test1(){
+        Response response = RestAssured.get("/regions");
+
+        System.out.println("response.statusCode() = " + response.statusCode());
+    }
 }
