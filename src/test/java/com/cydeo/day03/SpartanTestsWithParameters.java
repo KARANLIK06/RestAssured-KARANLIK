721d6c247ad85988c49b3f367c858d715f99b732
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-04 00:45:05 +0300

    day03

---
+++ src/test/java/com/cydeo/day03/SpartanTestsWithParameters.java
@@ -0,0 +1,71 @@
+package com.cydeo.day03;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class SpartanTestsWithParameters {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+
+        baseURI = "http://54.145.80.239:8000";
+    }
+
+     /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload
+       */
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 5)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(200,response.statusCode());
+
+        assertEquals("application/json", response.contentType());
+        assertTrue(response.body().asString().contains("Blythe"));
+    }
+
+      /*
+        TASK
+        Given accept type is Json
+        And Id parameter value is 500
+        When user sends GET request to /api/spartans/{id}
+        Then response status code should be 404
+        And response content-type: application/json
+        And "Not Found" message should be in response payload
+     */
+
+    @DisplayName("GET request /api/spartans/{id} Negative Test")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 500)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(404,response.statusCode());
+        assertEquals("application/json",response.contentType());
+        assertTrue(response.body().asString().contains("Not Found"));
+
+
+    }
+
+
+
+    }
