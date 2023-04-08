ef0357f2cfbfe23ca3c51e997637afceb82cda98
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-08 12:43:24 +0300

    day04

---
+++ src/test/java/com/cydeo/day04/SpartanWithJsonPath.java
@@ -0,0 +1,16 @@
+package com.cydeo.day04;
+
+import com.cydeo.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class SpartanWithJsonPath extends SpartanTestBase {
+    @DisplayName("Get one spartan with JsonPath")
+    @Test
+    public void test1(){
+
+    }
+}
