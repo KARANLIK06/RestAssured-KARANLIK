f2dd0c301249d12b9a70ee2310f87069e2481be4
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-08 21:19:10 +0300

    day05_HamcrestMatchers

---
+++ src/test/java/com/cydeo/day05/HamcrestMatchersIntro.java
@@ -0,0 +1,34 @@
+package com.cydeo.day05;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class HamcrestMatchersIntro {
+    @Test
+    public void simleTest1() {
+
+        // actual 5+5
+        assertThat(5 + 5, is(10));
+        assertThat(5 + 5, equalTo(10));
+        //matchers has 2 overloaded version
+        //first that accept actual value
+        //second that accept another matchers
+        //below examples is method is accepting another matchers equal to make it readable
+        assertThat(5 + 5, is(equalTo(10)));
+
+        assertThat(5 + 5, not(9));
+        assertThat(5 + 5, is(not(9)));
+        assertThat(5 + 5, is(not(equalTo(9))));
+
+
+        //number comparison
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan()
+        //lessThanOrEqualTo()
+        assertThat(5 + 5, is(greaterThan(9)));
+    }
+}
