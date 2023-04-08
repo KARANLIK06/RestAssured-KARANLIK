e38553a67c8c3d61c07eb55d5a8352b8e93a853c
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-08 12:38:23 +0300

    day04

--- src/test/java/com/cydeo/utilities/HRTestBase.java
+++ src/test/java/com/cydeo/utilities/HRTestBase.java
@@ -10,7 +10,7 @@ public class HRTestBase {
     @BeforeAll
     public static void init(){
 
-        // save baseURL inside this variable so that we dont need to type each http method.
+        // save baseURL inside this variable so that we don't need to type each http method.
         baseURI="http://54.145.80.239:1000/ords/hr";
 
         //get ip address from configurations
