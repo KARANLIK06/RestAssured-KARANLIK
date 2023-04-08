13c91cc23f74f4fcf91a3d1489ce0e7dd93aec55
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-08 21:34:25 +0300

    day05_HamcrestMatchers

--- src/test/java/com/cydeo/day05/HamcrestMatchersIntro.java
+++ src/test/java/com/cydeo/day05/HamcrestMatchersIntro.java
@@ -2,7 +2,11 @@ package com.cydeo.day05;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -31,4 +35,60 @@ public class HamcrestMatchersIntro {
         //lessThanOrEqualTo()
         assertThat(5 + 5, is(greaterThan(9)));
     }
+
+    @DisplayName("Assertion with String")
+    @Test
+    public void stringHamcrest() {
+
+        String text = "B22 is learning Hamcrest";
+
+        //checking for equality is same as numbers
+
+        assertThat(text,is("B22 is learning Hamcrest"));
+        assertThat(text,equalTo("B22 is learning Hamcrest"));
+        assertThat(text,is(equalTo("B22 is learning Hamcrest")));
+
+        //check if this text starts with B22
+        assertThat(text,startsWith("B22"));
+        //now do it in case insensitive manner
+        assertThat(text,startsWithIgnoringCase("b22"));
+        //endswith
+        assertThat(text,endsWith("rest"));
+
+        //check if text contains String learning
+        assertThat(text,containsString("learning"));
+        //with ignoring case
+        assertThat(text,containsStringIgnoringCase("LEARNING"));
+
+        String str ="  ";
+
+        //check if above str is blank
+        assertThat(str,blankString());
+
+        //check if trimmed str is empty string
+        assertThat(str.trim(),emptyString());
+    }
+
+    @DisplayName("Hamcrest for Collection")
+    @Test
+    public void testCollection(){
+
+        List<Integer> listOfNumbers = Arrays.asList(1,4,5,6,32,54,66,77,45,23);
+
+        //check size of the list
+        assertThat(listOfNumbers,hasSize(10));
+
+        //check if this list hasItem 77
+        assertThat(listOfNumbers,hasItem(77));
+
+        //check if this list hasItems 77,54,23
+        assertThat(listOfNumbers,hasItems(77,54,23));
+
+
+        //check if all numbers greater than 0
+        assertThat(listOfNumbers,everyItem(greaterThan(0)));
+
+
+    }
+
 }
