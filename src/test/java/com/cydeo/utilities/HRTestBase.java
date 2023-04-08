1f168f7619f1583561df13e9bcb705849ff9be01
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-08 11:46:15 +0300

    day03

---
+++ src/test/java/com/cydeo/utilities/HRTestBase.java
@@ -0,0 +1,18 @@
+package com.cydeo.utilities;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class HRTestBase {
+    @BeforeAll
+    public static void init(){
+
+        // save baseURL inside this variable so that we dont need to type each http method.
+        baseURI="http://54.145.80.239:1000/ords/hr";
+
+        //get ip address from configurations
+    }
+}
