90ac6d1d74898601cd9c4ae9234f717706ceeb85
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-09 11:52:51 +0300

    day05_ORDSHamcrestTest

--- src/test/java/com/cydeo/day05/ORDSHamcrestTest.java
+++ src/test/java/com/cydeo/day05/ORDSHamcrestTest.java
@@ -1,8 +1,12 @@
 package com.cydeo.day05;
 
 import com.cydeo.utilities.HRTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -20,11 +24,22 @@ public class ORDSHamcrestTest extends HRTestBase {
         //verify first names are .... (find proper method to check list against list)
         //verify emails without checking order (provide emails in different order,just make sure it has same emails)
         //expected names
+        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
 
 
-
-
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\":\"IT_PROG\"}")
+        .when()
+                .get("/employees")
+        .then()
+                .statusCode(200)
+                .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .body("items.first_name",containsInRelativeOrder("Alexander","Bruce","David","Valli","Diana")) //contains with order // will be same order
+                .body("items.email",containsInAnyOrder("VPATABAL","DAUSTIN","BERNST","AHUNOLD","DLORENTZ")) //contains without order
+                .body("items.first_name", equalTo(names)); // equality of lists assertion
     }
+
+
 
 
 }
