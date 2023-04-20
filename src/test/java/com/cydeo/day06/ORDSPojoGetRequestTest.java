31d7eb92d464812df4a8861946e0aafaaedc9d7e
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-04-20 21:46:48 +0300

    day06_Spartan.java

---
+++ src/test/java/com/cydeo/day06/ORDSPojoGetRequestTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.day06;
+
+import com.cydeo.utilities.HRTestBase;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+public class ORDSPojoGetRequestTest extends HRTestBase {
+
+    @Test
+    public void regionTest(){
+
+
+
+    }
+}
